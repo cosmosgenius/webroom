9880e43fbe23755d86d4f063f51c7bf88107faa1
Sharat M R <cosmosgenius@gmail.com>
2013-08-18 22:53:50 +0530

    add code for heroku and did some clean up

--- webroom.js
+++ webroom.js
@@ -8,6 +8,11 @@
     app.set('view engine', 'ejs');
     app.use('/static',express.static(__dirname + "/static"));
 
+    io.configure(function(){
+        io.set("transports",["xhr-polling"]);
+        io.set("polling duration",10);
+    });
+
     usernames={'length':0};
     app.get('/',function(req, res){
         var param = {
@@ -25,7 +30,7 @@
         usernames[socket.id] = {'uname': "user"+usernames.length};
         usernames.length +=1;
         socket.emit("uname",usernames[socket.id].uname);
-        socket.on("message",handle_message);
+        socket.on("wbmsg",handle_message);
 
         socket.on("changeuname",function(uname){
             for(var x in username){
@@ -44,7 +49,7 @@
 
         function handle_message(packet){
             packet['from']=usernames[socket.id].uname;
-            socket.broadcast.emit('message',packet);
+            socket.broadcast.emit('wbmsg',packet);
         }
     });
 
