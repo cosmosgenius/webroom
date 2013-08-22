b2d779ca183571d113e77814f86a18bc8116ec96
Sharat M R <cosmosgenius@gmail.com>
2013-08-23 00:19:51 +0530

    added border radius and remove function enclosure

--- webroom.js
+++ webroom.js
@@ -1,57 +1,53 @@
-(function(){
-    var express = require('express'),
-        app = express(),
-        server = require('http').createServer(app),
-        io = require('socket.io').listen(server);
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 
-    app.use(express.logger('dev'));
-    app.set('view engine', 'ejs');
-    app.use('/static',express.static(__dirname + "/static"));
+app.use(express.logger('dev'));
+app.set('view engine', 'ejs');
+app.use('/static',express.static(__dirname + "/static"));
 
-    io.configure(function(){
-        io.set("transports",["xhr-polling"]);
-        io.set("polling duration",10);
-    });
+io.configure(function(){
+    io.set("transports",["xhr-polling"]);
+    io.set("polling duration",10);
+});
 
-    usernames={'length':0};
-    app.get('/',function(req, res){
-        var param = {
-            room_title:'Webroom dev',
-            title:'Room 1'
-        };
-        res.render('index',param);
-    });
-
-    module.exports = function(config){
-        return server;
+usernames={'length':0};
+app.get('/',function(req, res){
+    var param = {
+        room_title:'Webroom dev',
+        title:'Room 1'
     };
+    res.render('index',param);
+});
 
-    io.sockets.on("connection",function(socket){
-        usernames[socket.id] = {'uname': "user"+usernames.length};
-        usernames.length +=1;
-        socket.emit("uname",usernames[socket.id].uname);
-        socket.on("wbmsg",handle_message);
+module.exports = function(config){
+    return server;
+};
 
-        socket.on("changeuname",function(uname){
-            for(var x in username){
-                if(username[x].uname == uname){
-                    socket.emit('system','Username Exist');
-                    return;
-                }
+io.sockets.on("connection",function(socket){
+    usernames[socket.id] = {'uname': "user"+usernames.length};
+    usernames.length +=1;
+    socket.emit("uname",usernames[socket.id].uname);
+    socket.on("wbmsg",handle_message);
+
+    socket.on("changeuname",function(uname){
+        for(var x in username){
+            if(username[x].uname == uname){
+                socket.emit('system','Username Exist');
+                return;
             }
-            usernames[socket.id].uname = uname;
-        });
-
-        socket.on("disconnect",function(val){
-            delete usernames[socket.id];
-            usernames.length -= 1;
-        });
-
-        function handle_message(packet){
-            packet['from']=usernames[socket.id].uname;
-            socket.broadcast.emit('wbmsg',packet);
         }
+        usernames[socket.id].uname = uname;
     });
 
-    
-})();
+    socket.on("disconnect",function(val){
+        delete usernames[socket.id];
+        usernames.length -= 1;
+    });
+
+    function handle_message(packet){
+        packet['from']=usernames[socket.id].uname;
+        socket.broadcast.emit('wbmsg',packet);
+    }
+});
